dc6dcdc0096f60d9d7c92747a35164058c084a23
orenjiakira <orenjiakira@gmail.com>
2017-05-21 23:58:38 -0300

    Change back from debug home url

--- metainfo.lua
+++ metainfo.lua
@@ -3,6 +3,6 @@ print("READING METAINFO")
 
 return {
   title = "orenjiAkira",
-  -- home_url = "https://orenjiakira.github.io/site",
-  home_url = "/orenjiakira.github.io/site",
+  home_url = "https://orenjiakira.github.io/site",
+  -- home_url = "/orenjiakira.github.io/site",
 }
